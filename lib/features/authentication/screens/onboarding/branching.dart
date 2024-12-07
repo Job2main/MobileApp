6557778242863601e7594bfaf8bf22d1c43ab6b9
Valentin Py <valentin.py@epitech.eu>
2024-12-07 12:35:36 +0300

    Automatic fixes

--- lib/features/authentication/screens/onboarding/branching.dart
+++ lib/features/authentication/screens/onboarding/branching.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:job2main/common/styles/spacing_styles.dart';
 import 'package:job2main/features/authentication/controllers/usertype_controller.dart';
 import 'package:job2main/features/authentication/screens/common/header.dart';
-import 'package:job2main/features/authentication/screens/login/widgets/login_header.dart';
 import 'package:job2main/features/authentication/screens/signup/signup.dart';
 import 'package:job2main/utils/constants/colors.dart';
 import 'package:job2main/utils/constants/sizes.dart';
 import 'package:job2main/utils/constants/text_strings.dart';
-import 'package:job2main/utils/helpers/any_color_mode.dart';
 
 class BranchingScreen extends StatelessWidget {
   const BranchingScreen({super.key});
@@ -22,7 +19,8 @@ class BranchingScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            THeader(title: TTexts.onBoardingChoiceTitle, subtitle: TTexts.onBoardingChoiceSubTitle),
+            const THeader(
+                title: TTexts.onBoardingChoiceTitle, subtitle: TTexts.onBoardingChoiceSubTitle),
             const SizedBox(height: TSizes.spaceBtwSections),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
